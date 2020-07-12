4c7a26e59bf20af9bbb4285c8294fde0380ddfc9
Spencer Sellers <spencesellers@gmail.com>
2020-07-12 17:43:08 -0500

    Bring example code into Main

--- TimeTravel/Program.cs
+++ TimeTravel/Program.cs
@@ -5,19 +5,11 @@ namespace TimeTravel
 {
     public class Program
     {
-        public static void Main()
-        {
-            var answer = FindN().Result;
-            
-            Console.Out.WriteLine($"Answer is {answer}");
-            // Prints "Answer is 60"
-        }
-        
-        private static async Task<int> FindN()
+        public static async Task Main()
         {
             var answerFromFuture = new AnswerFromFuture();
             
-            // Pull a number that's going to magically avoid hitting .Nope()
+            // Resolve a number that's going to magically avoid hitting .Nope()
             var n = await answerFromFuture;
 
             if (n % 10 != 0)
@@ -30,7 +22,8 @@ namespace TimeTravel
                 answerFromFuture.Nope();
             }
 
-            return n;
+            Console.Out.WriteLine($"Answer is {n}");
+            // Prints "Answer is 60"
         }
     }
 }
